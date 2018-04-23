40bd21c8a19e6c718e952434067a0e44a3170211
LK Loh <lkloh2410@gmail.com>
2018-04-22 23:26:59 -0700

    Broken window

---
+++ _posts/2018-04-22-broken-window.js
@@ -0,0 +1,27 @@
+---
+type: post
+title: "Pragmatic programmer: Broken window"
+date: 2018-04-22
+---
+
+"Fix problems ASAP."
+
+## Broken Window Theory
+
+Researchers in urban studies have found that the fastest way to make a building decay
+is to leave a broken window unrepaired.
+It gives people the sense that the owners don't care about the building,
+so they stop caring as well and start littering, etc.
+Very soon, the building is damaged beyond repair and condemned. 
+
+## Fix broken windows in anything ASAP - even if all you do is board it up
+
+In software, don't leave bad code 
+(no tests, bad designs) hanging.
+Take some action to fix it, if only to leave a comment
+stating why you are leaving it like that and when you are going to fix it,
+to communicate that someone is on top of a bad situation.
+
+Neglect contributes to software rot faster than anything else.
+
+
